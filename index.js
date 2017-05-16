9dbd7e0ade7cc353d4de82651a41f2c083997f05
sungwon.cha <mygenie3@naver.com>
2017-05-16 18:07:48 +0900

    use es5 syntax

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const helpers = require('./lib');
+'use strict';
 
-helpers.register = (Handlebars, namespaces) => {
-  const targets = namespaces ? [].concat.apply([], [namespaces])
-    .filter(e => e && typeof helpers[e] === 'object') : null;
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var helpers = require('./lib');
+
+helpers.register = function (Handlebars, namespaces) {
+  var targets = namespaces ? [].concat.apply([], [namespaces]).filter(function (e) {
+    return e && _typeof(helpers[e]) === 'object';
+  }) : null;
 
   if (!Handlebars || typeof Handlebars.registerHelper !== 'function') {
     throw new Error('Handlebars.registerHelper is undefined.');
   }
 
-  Object.keys(helpers).forEach((e) => {
-    if (typeof helpers[e] !== 'object') {
+  Object.keys(helpers).forEach(function (e) {
+    if (_typeof(helpers[e]) !== 'object') {
       return;
     }
     if (targets === null || targets.indexOf(e) !== -1) {
-      Object.keys(helpers[e]).forEach((name) => {
+      Object.keys(helpers[e]).forEach(function (name) {
         if (typeof helpers[e][name] === 'function' && !(name in Handlebars.helpers)) {
           Handlebars.registerHelper(name, helpers[e][name]);
         }
